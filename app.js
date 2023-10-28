92f3e4d7f7d5bb18c3b4bc165aba4582013c9db3
abeshahsan <abeshahsan2002@gmail.com>
2023-10-28 21:26:18 +0600

    added ids in html rows

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
         req.session.allAudio = []
         result.forEach(function (row) {
             req.session.allAudio.push({
+                id: row.AUDIO_ID,
                 title: (row.AUDIO_NAME ? row.AUDIO_NAME : "unknown"),
                 creator: (row.CREATOR_NAME? row.CREATOR_NAME : "unknown"),
                 length: "00:00"
