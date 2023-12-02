c46f7dd6cebfc704d7bbecd28434e56c47ee4e69
abeshahsan <abeshahsan2002@gmail.com>
2023-12-02 22:19:56 +0600

    added context menu

--- public/javascripts/audio-table.js
+++ public/javascripts/audio-table.js
@@ -5,16 +5,16 @@ $(document).ready(function () {
     let playPause = $(".icons.play-pause");
     let currentTrack = document.querySelector("#current-track");
     let currentSrc = document.querySelector("#current-src");
-    
+
     let playingSongId = localStorage.getItem("song-id");
     let volumeValue = localStorage.getItem("volume-value");
     let manualSeek = false;
-    let seekSlider = $("#seek-slider" );
-    
+    let seekSlider = $("#seek-slider");
+
     $(".audio-row").each(function (i, element) {
-        if(playingSongId == $(element).attr("id")){
-            if(localStorage.getItem("page-reloaded") == 1){
-                localStorage.setItem("page-reloaded", 0);
+        if (playingSongId == $(element).attr("id")) {
+            if (localStorage.getItem("page-reloaded") == "1") {
+                localStorage.setItem("page-reloaded", "0");
                 currentSrc.setAttribute("src", "/song/" + playingSongId);
                 currentTrack.load();
             }
@@ -22,23 +22,44 @@ $(document).ready(function () {
             selectedRow = element;
             updateSeekSlider();
         }
-        $(element).on("click", function () {
-            playAudio($(element))
+        $(element).on("mouseup", function (event) {
+            console.log(event.which)
+            switch (event.which) {
+                case 1:
+                    playAudio($(element))
+                    break;
+            }
         })
-    })
+        $.contextMenu({
+            selector: '.audio-row',
+            className: 'audio-context-menu',
+            callback: function (key, options) {
 
-    function updateSeekSlider(){
-        currentTrack.onloadedmetadata = function() {
-            $("#seek-slider").slider("option", { max: Math.floor(currentTrack.duration) });
+            },
+            items: {
+                "edit": {name: "Edit", icon: "edit"},
+                "cut": {name: "Cut", icon: "cut"},
+                copy: {name: "Copy", icon: "copy"},
+                "paste": {name: "Paste", icon: "paste"},
+                "delete": {name: "Delete", icon: "delete"},
+            }
+        });
+    });
+
+
+    function updateSeekSlider() {
+        currentTrack.onloadedmetadata = function () {
+            $("#seek-slider").slider("option", {max: Math.floor(currentTrack.duration)});
             currentTrack.currentTime = localStorage.getItem("audio-current-time");
         };
     }
-    
-    window.onbeforeunload = function reloadHandler(){
-        localStorage.setItem("page-reloaded",1);
-        localStorage.setItem("volume-value",volumeValue);
-        localStorage.setItem("audio-current-time",currentTrack.currentTime);
+
+    window.onbeforeunload = function reloadHandler() {
+        localStorage.setItem("page-reloaded", "1");
+        localStorage.setItem("volume-value", volumeValue);
+        localStorage.setItem("audio-current-time", currentTrack.currentTime);
     }
+
     function playAudio(row) {
         if (selectedRow) {
             $(selectedRow).removeClass('selected');
@@ -46,20 +67,20 @@ $(document).ready(function () {
         $(row).addClass('selected');
         selectedRow = row
         let songId = $(row).attr("id");
-        
+
         currentSrc.setAttribute("src", "/song/" + songId);
         currentTrack.load();
         currentTrack.volume = (volumeValue / 100);
-        currentTrack.duration = 
-        currentTrack.play();
+        currentTrack.duration =
+            currentTrack.play();
         currentTrack.addEventListener("ended", function () {
             togglePlayPause()
         })
         setIsPlaying(1);
-        localStorage.setItem("song-id",songId);
+        localStorage.setItem("song-id", songId);
         updateSeekSlider();
     }
-    
+
     function setIsPlaying(_isPlaying) {
         isPlaying = _isPlaying;
         $(I_PlayPause).removeClass("fa-play fa-pause");
@@ -69,7 +90,7 @@ $(document).ready(function () {
             $(I_PlayPause).addClass("fa-play");
         }
     }
-    
+
     function togglePlayPause() {
         if (isPlaying) {
             currentTrack.pause();
@@ -78,55 +99,50 @@ $(document).ready(function () {
         }
         setIsPlaying(!isPlaying);
     }
-    
+
     $(playPause).on("click", function () {
         togglePlayPause()
     });
-    
-    // $(playPause).on("keypress", function () {
-        //     if( e.key == " ") togglePlayPause()
-        // });
-        
-        
-        $("#volume-slider").slider({
-            value  : volumeValue,
-            step   : 0.01,
-            range  : 'min',
-            min    : 0,
-            max    : 100,
-            change : function(){
-                let value = $("#volume-slider").slider("value");
-                volumeValue = value;
-                currentTrack.volume = (value / 100);
-            },
-            slide : function(){
-                let value = $("#volume-slider").slider("value");
-                volumeValue = value;
-                currentTrack.volume = (value / 100);
-            }
-        });
-        $(seekSlider).slider({
-            value: currentTrack.currentTime,
-            range: "min",
-            min: 0,
-            // max: 100,
-            step: 0.1,
-            // animate: true,
-            slide:  function(event, ui) {
-                // console.log($("#seek-slider").slider("option", "max"));
-                manualSeek = true;
-            },
-            stop: function() {
-                currentTrack.currentTime = $(seekSlider).slider("value");
-                manualSeek = false; 
-            }
-        }); 
 
-        $(currentTrack).on('timeupdate', function() {
-            if (!manualSeek) {
-              $(seekSlider).slider('value', currentTrack.currentTime);
-            }
-        });
+    $("#volume-slider").slider({
+        value: volumeValue,
+        step: 0.01,
+        range: 'min',
+        min: 0,
+        max: 100,
+        change: function () {
+            let value = $("#volume-slider").slider("value");
+            volumeValue = value;
+            currentTrack.volume = (value / 100);
+        },
+        slide: function () {
+            let value = $("#volume-slider").slider("value");
+            volumeValue = value;
+            currentTrack.volume = (value / 100);
+        }
+    });
+    $(seekSlider).slider({
+        value: currentTrack.currentTime,
+        range: "min",
+        min: 0,
+        // max: 100,
+        step: 0.1,
+        // animate: true,
+        slide: function (event, ui) {
+            // console.log($("#seek-slider").slider("option", "max"));
+            manualSeek = true;
+        },
+        stop: function () {
+            currentTrack.currentTime = $(seekSlider).slider("value");
+            manualSeek = false;
+        }
+    });
+
+    $(currentTrack).on('timeupdate', function () {
+        if (!manualSeek) {
+            $(seekSlider).slider('value', currentTrack.currentTime);
+        }
+    });
 })
     
     
