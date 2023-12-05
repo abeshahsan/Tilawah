91a280b3f58b42514a67088aea7fc8ab5e42e5ae
abeshahsan <abeshahsan2002@gmail.com>
2023-12-05 22:07:41 +0600

    audio loads before server satrts

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
 const logger = require('morgan');
 const database = require('./database');
+const controls = require('./routes/controls');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -22,87 +23,59 @@ app.use(expressSession({
     secret: "kaane-kaane-boli-shuno", resave: true, saveUninitialized: true,
 }));
 
-/**
- * The server should load all the audio from the database.
- * Store it into session to use it globally
- */
-app.use((req, res, next) => {
-    if (req.session.allAudio) {
-        next()
-        return;
-    }
+(async () => {
+    try {
+        const allAudio = await controls.preLoadAllAudio();
+        // Do something with the allAudio object
+        console.log(Object.keys(allAudio).length + " audio loaded");
 
-    // console.log('lol')
-    database.loadAllAudios(async function (result) {
-        req.session.allAudio = {};
-
-        try {
-            const durations = await Promise.all(result.map(async (row) => {
-                try {
-                    const duration = await getAudioDurationInSeconds(`${row.PATH}`, ffProbeStatic.path);
-                    return utility.timeFormatFromSeconds(duration);
-                } catch (err) {
-                    console.log(err.message);
-                    return null; // or some placeholder value
-                }
-            }));
-
-            result.forEach((row, index) => {
-                // console.log("lol")
-                if (durations[index]) {
-                    req.session.allAudio[row.AUDIO_ID] = {
-                        id: row.AUDIO_ID,
-                        title: row.AUDIO_NAME || "unknown",
-                        creator: row.CREATOR_NAME || "unknown",
-                        collection: row.COLLECTION_NAME || "unknown",
-                        length: durations[index] || "unknown", // Use the corresponding duration
-                        path: row.PATH,
-                    };
-                }
-                if (index + 1 === result.length) {
-                    next();
-                }
-            });
-        } catch (error) {
-            console.error(error);
-        }
-    });
-
-})
+        /**
+         * The server should load all the audio from the database.
+         * Store it into session to use it globally
+         */
+        app.use((req, res, next) => {
+            req.session.allAudio = allAudio;
+            next();
+        })
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+        app.set('views', path.join(__dirname, 'views'));
+        app.set('view engine', 'ejs');
 
 // app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+        app.use(express.json());
+        app.use(express.urlencoded({extended: false}));
+        app.use(cookieParser());
+        app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', indexRouter);
-// app.use('/', usersRouter);
-app.use('/', loginRouter);
-app.use('/', registerRouter);
-app.use('/', profileRouter);
+        app.use('/', indexRouter);
+        app.use('/', loginRouter);
+        app.use('/', registerRouter);
+        app.use('/', profileRouter);
+        app.use('/', usersRouter);
 
 
-app.use(function (req, res, next) {
-    next(createError(404))
-});
+        app.use(function (req, res, next) {
+            next(createError(404))
+        });
 // catch 404 and forward to error handler
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+        app.use(function (err, req, res, next) {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+            // render the error page
+            res.status(err.status || 500);
+            res.render('error');
+        });
 
+    } catch (error) {
+        // Handle any errors that occurred during the loading process
+        console.error("Error loading audio:", error);
+    }
+})();
 
 module.exports = app;
